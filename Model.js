35b5def6ae1cf76284e5c45b6fdd218e385b7322
mohdashiq18 <moashiq2018@gmail.com>
2023-06-09 23:54:49 +0530

    hack

--- Model.js
+++ Model.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 const ipSchema=mongoose.Schema({
     ipAdresss:{type:String,require:true},
-    mac:{type:String,require:true}
+    macAddress:{type:String,require:true}
 })
 
 const IpModel=mongoose.model("ipAddress",ipSchema)
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const {IpModel} =require("./Model")
+const { IpModel } = require('./Model');
 const app = express();
-const {connect} =require("./Config")
+const { connect } = require('./Config');
 const request = require('request');
 const axios = require('axios');
 const { exec } = require('child_process');
 
-// Execute the "ipconfig" command to get the MAC address
-let mac;
+// Connect to MongoDB
+connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
+// Get MAC address
+let mac = '';
 
- 
 // Replace with the IP address you want to retrieve the location for
-app.get("/",async(req,res)=>{
-  try{
-    exec('ipconfig /all', (error, stdout, stderr) => {
+app.get('/', async (req, res) => {
+  try {
+    exec('ifconfig | grep -o -E "([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}" | head -n 1', (error, stdout, stderr) => {
       if (error) {
         console.error(`Error executing command: ${error.message}`);
         return;
@@ -23,57 +30,48 @@ app.get("/",async(req,res)=>{
         console.error(`Command stderr: ${stderr}`);
         return;
       }
-    
+
       // Extract the MAC address from the output
-      const macAddressRegex = /Physical Address[^\r\n]*: ([0-9A-Fa-f:-]{17})/g;
-      const macAddresses = stdout.match(macAddressRegex);
-    
-      // Print the MAC addresses
-     mac=macAddresses[0]
-    //   macAddresses.forEach((macAddress, index) => {
-    //     console.log(`${index + 1}. ${macAddress}`);
-    //   });
+      mac = stdout.trim();
+
+      // Print the MAC address
+      console.log('MAC address:', mac);
     });
-    
+
     const ipstackAccessKey = 'dcf307c13bc588c8d1651f6fd366ad17';
-    request('https://api.ipify.org?format=json',async (error, response, body) => {
+    request('https://api.ipify.org?format=json', async (error, response, body) => {
       if (error) {
         console.error('Error retrieving public IP address:', error);
       } else {
         const data = JSON.parse(body);
-        const publicIp = data.ip; 
-        axios.get(`http://api.ipstack.com/${publicIp}?access_key=${ipstackAccessKey}`)
-      .then(response => {
-        const locationData = response.data;
-        res.send(mac,locationData)
-        // Extract latitude and longitude from locationData and use them as needed
-      })
-      .catch(error => {
-        console.error('Error retrieving location data:', error);
-      });
-    
-        const Data=new IpModel({ipAdresss:publicIp,mac})
-        await Data.save()
+        const publicIp = data.ip;
+
+        axios
+          .get(`http://api.ipstack.com/${publicIp}?access_key=${ipstackAccessKey}`)
+          .then(async (response) => {
+            const locationData = response.data;
+            console.log('Location data:', locationData);
+
+            // Save IP and MAC address to MongoDB
+            const newData = new IpModel({ ipAddress: publicIp, macAddress: mac });
+            await newData.save();
+
+            res.json({ ipAddress: publicIp, macAddress: mac, locationData });
+          })
+          .catch((error) => {
+            console.error('Error retrieving location data:', error);
+            res.sendStatus(500);
+          });
+
         console.log('Public IP address:', publicIp);
       }
     });
-    
-     
+  } catch (error) {
+    console.error('Error:', error);
+    res.sendStatus(500);
   }
-  catch{
-    res.send("err")
-  }
-})
-
-
-app.listen(3000, async() => {
-    try{
-      await connect
-      console.log('Server running on port 3000');
-    }
-    catch{
-     
-    }
-  
 });
- 
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
